8e26b3be36275d0bc05fa5d4858bdcb68932719c
Paul <parmati94@gmail.com>
2024-09-11 16:43:28 -0400

    Update .db location

--- api/api/database.py
+++ api/api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./NotifyAgent.db"
+DATABASE_URL = "sqlite:///./api/config/NotifyAgent.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
